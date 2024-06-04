7217f9f0d72664b430911440f29e24f4b64fadce
fahadalijs <fahad9827022@gmail.com>
2024-06-05 02:04:05 +0500

    Synchronous vs Asynchronous

---
+++ app.js
@@ -0,0 +1,81 @@
+// console.log(Synchronous vs. Asynchronous);
+
+// console.log("Hello world");
+// console.log("Hello world-1");
+// console.log("Hello world-2");
+// console.log("Hello world-3");
+// hello()
+
+// function hello() {
+//     console.log("hello -4");
+// }
+// console.log("hello-5");
+
+
+
+//synchronous
+//asynchronous
+
+// console.log("hello -1");
+// console.log("hello -2");
+// console.log("hello -3");
+
+// setTimeout(() => {
+//     console.log("hello -4");
+// }, 0);
+
+// console.log("hello -5");
+
+
+// asynchronous
+
+//settimeout
+//setInterval
+//promises
+//async await
+//axios
+//fetch
+
+
+
+// Api
+
+// Application programming interface
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
